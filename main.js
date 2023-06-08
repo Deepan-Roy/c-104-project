e3db1522e0daa7ffd5f4d7d259f77be76a7d9dd6
Deepan-Roy <110827577+Deepan-Roy@users.noreply.github.com>
2023-06-08 14:18:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.attach( '#camera' );
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+function Capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML ='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+  
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kLLHk6Alz/model.json',modelLoaded);
